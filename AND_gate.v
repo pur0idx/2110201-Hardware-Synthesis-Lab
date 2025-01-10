a82936689ac33cf2f8730fc3124cb6f4cf7ecc4c
Pur0idx <peanutpeanut@hotmail.co.th>
2025-01-10 22:37:33 +0700

    Add: AND_gate

---
+++ AND_gate.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/10/2025 07:30:52 AM
+// Design Name: 
+// Module Name: AND_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AND_gate(
+    input A,
+    input B,
+    output out
+    );
+    
+    assign out = A ~| B;
+    
+endmodule
